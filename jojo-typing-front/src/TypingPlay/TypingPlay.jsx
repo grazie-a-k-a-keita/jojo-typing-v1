c15756c6f20d27c65708e333a42f55bd75260cb0
Ono　Keita <oh.kitbsk.1028@icloud.com>
2022-11-03 08:58:40 +0000

    feat: 残り時間のゲージを作成 #issue3

--- jojo-typing-front/src/TypingPlay/TypingPlay.jsx
+++ jojo-typing-front/src/TypingPlay/TypingPlay.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "../css/TypingPlay.css";
 
 const TypingPlay = (props) => {
+  //
   return (
     <>
       {props.showGame ? (
         <>
-          <div>タイピングゲーム</div>
-          <div>ゲージ</div>
+          <div id="game">タイピングゲーム部分</div>
+          <div className="timeGauge"></div>
         </>
       ) : (
         <></>
