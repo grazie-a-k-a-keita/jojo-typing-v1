e5b8dbadc42cb1c02e7930fe06c1dbf7e19f97e6
Ono　Keita <oh.kitbsk.1028@icloud.com>
2023-01-02 15:34:26 +0000

    feat: ヘッダー押下時の挙動をリロードに修正

--- jojo-typing-front/src/Components/header.jsx
+++ jojo-typing-front/src/Components/header.jsx
@@ -1,22 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  //
+  const navigate = useNavigate();
+  const reload = () => {
+    navigate("/");
+    window.location.reload();
+  };
+  //
   return (
     <>
       <header>
         <nav className="bg-sky-700 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-          <div className="flex flex-wrap justify-between items-center mx-auto">
-            <Link to="/" className="flex items-center">
+          <div
+            className="flex flex-wrap justify-between items-center mx-auto"
+            onClick={() => reload()}
+          >
+            <div className="flex items-center cursor-pointer">
               <img
                 src={`${process.env.PUBLIC_URL}/images/headerIcon.png`}
                 className="mr-3 h-6 sm:h-9"
                 alt="Flowbite Logo"
               />
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              <p className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 JOJOタイピング
-              </span>
-            </Link>
+              </p>
+            </div>
           </div>
         </nav>
       </header>
